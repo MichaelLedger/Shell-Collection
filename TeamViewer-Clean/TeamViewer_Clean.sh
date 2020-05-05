734eb07a06a2f10bc42b66781c2d10b16e5a1ca2
MichaelLedger <MichaelLedger@163.com>
2020-05-05 21:03:59 +0800

    TeamViewer-Clean.sh

---
+++ TeamViewer-Clean/TeamViewer_Clean.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# $ sudo find / -name *teamviewer*
+
+rm -rf /Library/Preferences/com.teamviewer.teamviewer.preferences.plist
+rm -rf /Library/PrivilegedHelperTools/com.teamviewer.Helper
+rm -rf /Library/LaunchAgents/com.teamviewer.teamviewer_desktop.plist
+rm -rf /Library/LaunchAgents/com.teamviewer.teamviewer.plist
+rm -rf /Library/LaunchDaemons/com.teamviewer.Helper.plist
+rm -rf /Library/LaunchDaemons/com.teamviewer.teamviewer_service.plist
+rm -rf /private/var/db/receipts/com.teamviewer.teamviewerAgent.bom
+rm -rf /private/var/db/receipts/com.teamviewer.teamviewerPriviledgedHelper.plist
+rm -rf /private/var/db/receipts/com.teamviewer.teamviewer.plist
+rm -rf /private/var/db/receipts/com.teamviewer.teamviewerRestarter.plist
+rm -rf /private/var/db/receipts/com.teamviewer.teamviewer.bom
+rm -rf /private/var/db/receipts/com.teamviewer.teamviewerPriviledgedHelper.bom
+rm -rf /private/var/db/receipts/com.teamviewer.teamviewerAuthPlugin.bom
+rm -rf /private/var/db/receipts/com.teamviewer.teamviewerAgent.plist
+rm -rf /private/var/db/receipts/com.teamviewer.teamviewerRestarter.bom
+rm -rf /private/var/db/receipts/com.teamviewer.teamviewerAuthPlugin.plist
+
