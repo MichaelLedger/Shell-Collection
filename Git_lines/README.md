252887871f8f13d0d35d74373db410d8e9513303
GavinXiang <gavin.xiang@planetart.cn>
2021-12-02 19:28:36 +0800

    [Git_lines] Update shell

--- Git_lines/README.md
+++ Git_lines/README.md
@@ -9,13 +9,13 @@
 ## 终端实例
 ```
 # 切换到Git项目目录下
-$ cd /Users/mxr/Desktop/4dbookcity
+$ cd ~/Desktop/repo_name
 # 执行脚本
-$ sh /Users/mxr/Desktop/XDS/git_lines.sh 2018 6 1 2018 6 30
-开始统计2018年6月1日至2018年6月30日每个人的代码行数...
+% sh ~/Downloads/Shell-Collection/Git_lines/git_lines.sh 2020 6 22 2021 12 2
+[repo_name]-开始统计2020年6月22日至2021年12月2日每个人的代码行数...
 结束统计:)
 旧版本的Excel不支持逗号分割符
-推荐使用Mac自带的Numbers打开《2018年6月1日至2018年6月30日开发行数统计.csv》
+推荐使用Mac自带的Numbers打开《[repo_name]-2020年6月22日至2021年12月2日开发行数统计.csv》
 ```
 
 
--- Git_lines/git_lines.sh
+++ Git_lines/git_lines.sh
@@ -6,11 +6,23 @@ if [ $# -ne 6 ]; then
     exit 1
 fi
 
-csv=./$1年$2月$3日至$4年$5月$6日开发行数统计_temp.csv
+# project_path=$(cd `dirname $0`; pwd)
+# project_name="${project_path##*/}"
 
-csv2=./$1年$2月$3日至$4年$5月$6日开发行数统计.csv
+# echo $project_path
+# echo $project_name
 
-echo "开始统计$1年$2月$3日至$4年$5月$6日每个人的代码行数..."
+# work_path=$(dirname $0)
+# echo ${work_path} 
+pwd_path=$(pwd) 
+# echo ${pwd_path}
+pwd_name="${pwd_path##*/}"
+
+csv=./[$pwd_name]-$1年$2月$3日至$4年$5月$6日开发行数统计_temp.csv
+
+csv2=./[$pwd_name]-$1年$2月$3日至$4年$5月$6日开发行数统计.csv
+
+echo "[$pwd_name]-开始统计$1年$2月$3日至$4年$5月$6日每个人的代码行数..."
 
 # 移除同名文件
 
@@ -20,20 +32,27 @@ rm -rf $csv $csv2
 
 # printf "\xEF\xBB\xBF" > $csv
 
-echo "$1年$2月$3日至$4年$5月$6日的开发行数,,," >> $csv
+echo "$1年$2月$3日至$4年$5月$6日的开发行数,,,," >> $csv
 
-echo "开发人员,增加行数,删除行数,合计" >> $csv
+echo "开发人员,增加行数,删除行数,差值,合计" >> $csv
 
 # 第一项($1)为增加的代码行数 第二项($2)为删除的代码行数 ($3)第三项为代码所在路径
 
-# git log --author=dashan.xiang --pretty=tformat: --numstat --since=$1-$2-$3 --until=$4-$5-$6 | awk '{add+=$1;del+=$2;total+=$1+$2}END{print "X大山,"add,",",del,",",total}' >> $csv
-git log --author=Mountain\ Xiang --pretty=tformat: --numstat --since=$1-$2-$3 --until=$4-$5-$6 | awk '{add+=$1;del+=$2;total+=$1+$2}END{print "X大山,"add,",",del,",",total}' >> $csv
+# git log --author={正则表达式是此过滤器的基础机制}
 
-git log --author=kfkf1127 --pretty=tformat: --numstat --since=$1-$2-$3 --until=$4-$5-$6 | awk '{add+=$1;del+=$2;total+=$1+$2}END{print "X峰,"add,",",del,",",total}' >> $csv
+git log --author=JayJiang --pretty=tformat: --numstat --since=$1-$2-$3 --until=$4-$5-$6 | awk '{add+=$1;del+=$2;diff+=$1-$2;total+=$1+$2}END{print "JayJiang,"add,",",del,",",diff,",",total}' >> $csv
 
-git log --author=weilong --pretty=tformat: --numstat --since=$1-$2-$3 --until=$4-$5-$6 | awk '{add+=$1;del+=$2;total+=$1+$2}END{print "X伟龙,"add,",",del,",",total}' >> $csv
+git log --author=Mike --pretty=tformat: --numstat --since=$1-$2-$3 --until=$4-$5-$6 | awk '{add+=$1;del+=$2;diff+=$1-$2;total+=$1+$2}END{print "Mike,"add,",",del,",",diff,",",total}' >> $csv
 
-git log --author=x\ d --pretty=tformat: --numstat --since=$1-$2-$3 --until=$4-$5-$6 | awk '{add+=$1;del+=$2;total+=$1+$2}END{print "X礼节,"add,",",del,",",total}' >> $csv
+git log --author=LaurenceFan --pretty=tformat: --numstat --since=$1-$2-$3 --until=$4-$5-$6 | awk '{add+=$1;del+=$2;diff+=$1-$2;total+=$1+$2}END{print "Laurence,"add,",",del,",",diff,",",total}' >> $csv
+
+git log --author=kellyxu8 --pretty=tformat: --numstat --since=$1-$2-$3 --until=$4-$5-$6 | awk '{add+=$1;del+=$2;diff+=$1-$2;total+=$1+$2}END{print "Kelly,"add,",",del,",",diff,",",total}' >> $csv
+
+git log --author="\(Watson\ Wang\)\|\(watsonPlanetart\)" --pretty=tformat: --numstat --since=$1-$2-$3 --until=$4-$5-$6 | awk '{add+=$1;del+=$2;diff+=$1-$2;total+=$1+$2}END{print "Watson,"add,",",del,",",diff,",",total}' >> $csv
+
+git log --author=samuelc --pretty=tformat: --numstat --since=$1-$2-$3 --until=$4-$5-$6 | awk '{add+=$1;del+=$2;diff+=$1-$2;total+=$1+$2}END{print "Samuel,"add,",",del,",",diff,",",total}' >> $csv
+
+git log --author=GavinXiang --pretty=tformat: --numstat --since=$1-$2-$3 --until=$4-$5-$6 | awk '{add+=$1;del+=$2;diff+=$1-$2;total+=$1+$2}END{print "Gavin,"add,",",del,",",diff,",",total}' >> $csv
 
 # iconv文件字符转码为GB18030以支持Excel,避免中文乱码
 
@@ -47,7 +66,7 @@ echo "结束统计:)"
 
 echo "旧版本的Excel不支持逗号分割符"
 
-echo "推荐使用Mac自带的Numbers打开《$1年$2月$3日至$4年$5月$6日开发行数统计.csv》"
+echo "推荐使用Mac自带的Numbers打开《[$pwd_name]-$1年$2月$3日至$4年$5月$6日开发行数统计.csv》"
 
 # 使用Mac自带的Numbers打开csv文件
 
